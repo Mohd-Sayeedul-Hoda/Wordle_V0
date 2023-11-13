9969dde8db49a95f223e1345e780e2b9336e3949
Mohd Sayeedul Hoda <sayeedulhoda@gmail.com>
2023-11-13 22:22:16 +0530

    completed the half parted of UI

--- index.js
+++ index.js
@@ -1,15 +1,80 @@
-/* 
-- you provide five char guesses
-- each guess must be a word
-- when you press enter,
-      - board
-	- you see the status of last guess
-	- letters that are in wrong palces are green 
-	- missing letters are grey
-	- correct guesses + poistionn are green
-      - keyboard
-      	-keyboard reflect same data
-- you ony see past + active attemnpt
-- if you make too many attempts you probably die
-*/
+'use strict'
+
+let grid = document.getElementById('grid')    
+
+function buildGrid(){
+	for (let i=0; i<6; i++){
+		let row = document.createElement('div')
+		for (let j=0; j<5;j++){
+			let cell = document.createElement('div')
+			cell.className='cell'
+			row.appendChild(cell)
+			cell.textContent= ''
+		}
+	grid.appendChild(row)
+	}
+}
+buildGrid();
+
+let wordList= [
+	//'patio',
+	//'darts',
+	//'piano',
+	'horse',
+];
+
+let randomIndex= Math.floor(Math.random()*wordList.length)
+let secret = wordList[randomIndex]
+
+let attempts = ['rohan','wowzy','horse']
+let currentAttempt=''
+
+updateGrid()
+
+function updateGrid(){
+	let row = grid.firstChild
+	for (let attempt of attempts){
+		drawPastAttempt(row,attempt)
+		row=row.nextSibling
+	}
+	drawCurrentAttempt(row,currentAttempt)
+	row = row.nextSibling
+}
+
+function drawPastAttempt(row, attempt){
+	for (let i = 0; i<5 ; i++){
+		let cell = row.children[i]
+		if (attempt[i] !== undefined){
+			cell.textContent = attempt[i]  
+		}
+		else{
+			cell.innerHTML = '<div style="opacity:0">X</div>'
+		}
+		cell.style.background = getByColor(attempt,i)
+		
+	}
+}
+
+function drawCurrentAttempt(row, attempt){
+	for (let i = 0; i<5 ; i++){
+		let cell = row.children[i]
+		if (attempt[i] !== undefined){
+			cell.textContent = attempt[i] 
+		}else{
+			cell.innerHTML = '<div style="opacity: 0">X</div>'
+		}
+	}
+}
+
+function getByColor(attempt,i){
+	let correctLetter = secret[i]
+	let attemptLetter = attempt[i]
+	if (attemptLetter ===undefined || secret.indexOf(attemptLetter) === -1){
+		return '#212121'
+	}
+	if (correctLetter === attemptLetter){
+		return '#538d4e'
+	}
+	return '#b59f3b'
+}
 
