9abc549521e68f0bf7311281df8ef9d974348c12
Mohd Sayeedul Hoda <sayeedulhoda@gmail.com>
2023-11-13 13:00:33 +0530

    started the project

---
+++ index.js
@@ -0,0 +1,15 @@
+/* 
+- you provide five char guesses
+- each guess must be a word
+- when you press enter,
+      - board
+	- you see the status of last guess
+	- letters that are in wrong palces are green 
+	- missing letters are grey
+	- correct guesses + poistionn are green
+      - keyboard
+      	-keyboard reflect same data
+- you ony see past + active attemnpt
+- if you make too many attempts you probably die
+*/
+
